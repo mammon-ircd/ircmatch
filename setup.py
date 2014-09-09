96ffa8c6d6d2c97033e29387c90863a851c200ce
William Pitcock <nenolod@dereferenced.org>
2014-09-09 16:35:21 -0500

    enhance setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ setup (
     name='ircmatch',
     version='1.0',
     ext_modules=[Extension('ircmatch', ['ircmatch.c', 'ircmatch_python.c'])],
+    description='library for matching IRC masks based on atheme',
+    author='William Pitcock',
+    author_email='nenolod@dereferenced.org',
+    url='http://kaniini.dereferenced.org/projects/ircmatch.html'
 )
 
