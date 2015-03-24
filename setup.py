a5d65993348f7ee0671a063707034101fab29859
William Pitcock <nenolod@dereferenced.org>
2015-03-24 14:32:02 -0500

    ircmatch 1.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 setup (
     name='ircmatch',
-    version='1.1',
+    version='1.2',
     ext_modules=[Extension('ircmatch', ['ircmatch.c', 'ircmatch_python.c'])],
     description='library for matching IRC masks based on atheme',
     author='William Pitcock',
